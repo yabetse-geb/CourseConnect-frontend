b18a55617ec5b26384a39832949715e46d38ebfb
bzgrey <bennyzgrey@gmail.com>
2025-11-23 18:30:54 -0500

    remove about page

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,15 +11,6 @@ const router = createRouter({
       meta: { requiresAuth: true },
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-      meta: { requiresAuth: true },
-    },
-    {
       path: '/auth',
       name: 'auth',
       component: () => import('../views/AuthView.vue'),
